ff6580d58d3f2f23508edd7ed81928403eccaddc
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-04-17 14:26:24 -0400

    updates

---
+++ app/scripts/data.js
@@ -0,0 +1,8 @@
+var joe = new Soldier("joe", "allies");
+var hanz = new Soldier("hanz", "axis");
+var pistol = new Weapon("pistol", "small", 20);
+var grenade = new Weapon("grenade", "small", 30);
+var tank = new Weapon("tank", "large", 40);
+var germany = new Battlefield("forest", 5);
+var france = new Battlefield("beach", 10);
+var egypt= new Battlefield("desert", 15);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,51 +3,51 @@ function play(index) {
 	alert("The setting is WWII. Choose a soldier, weapon, and battlefield.");
 };
 
-///////////////////////////////////////
+///////////////////////////////////////=
 
 function Soldier(name, side) {
 	this.name = name;
 	this.side = side;
 	this.life = 100;
-	this.addDamage = function(weapon) {
-		if (this.life <= 0) {
-			console.log("dead");
-			alert(this.name + " is dead. Game over!");
-		}
-	};
 }
+
+Soldier.prototype.addDamage = function(weapon) {
+	if (this.life <= 0) {
+	console.log("dead");
+	alert(this.name + " is dead. Game over!");
+	}
+}
+
 
 function Weapon(type, size, damage) {
 	this.type = type || "fists";
 	this.size = size;
 	this.damage = damage;
-	this.fire = function(soldier) {
-		console.log("Bam!");
-		// alert("fire away!");
-			soldier.life = (soldier.life - this.damage);
-			soldier.addDamage(this);
-		}
-	};
 
+}
+
+Weapon.prototype.fire = function(soldier) {
+	console.log("Bam!");
+	// alert("fire away!");
+	soldier.life = (soldier.life - this.damage);
+	soldier.addDamage(this);
+}
+	
 
 function Battlefield(landType, weatherDamage) {
 	this.landType = landType;
 	this.weatherDamage = weatherDamage;
-	this.element = function(weapon) {
-		console.log("damage!");
-			weapon.damage = (weapon.damage - weatherDamage);
-	}
-};
+}
+	
+Battlefield.prototype.element = function(weapon) {
+	console.log("weapon is damaged!");
+	weapon.damage = (weapon.damage - this.weatherDamage);
+}
 
 
-var joe = new Soldier("joe", "allies");
-var hanz = new Soldier("hanz", "axis");
-var pistol = new Weapon("pistol", "small", 20);
-var grenade = new Weapon("grenade", "small", 30);
-var tank = new Weapon("tank", "large", 40);
-var germany = new Battlefield("forest", 5);
-var france = new Battlefield("beach", 10);
-var egypt= new Battlefield("desert", 15);
+
+
+
 
 
  
